320497964160dd470e123744c5164b5b03178717
tianSHUTER <87591235+tianSHUTER@users.noreply.github.com>
2021-11-15 00:06:09 +0800

    增加必做题第二

---
+++ 必做题2/init.sql
@@ -0,0 +1,28 @@
+
+
+
+create schema demo_master;
+create schema demo_slave_0;
+create schema demo_slave_1;
+
+create table demo_master.users(id bigint, name varchar(8), comment varchar(16));
+create table demo_slave_0.users(id bigint, name varchar(8), comment varchar(16));
+create table demo_slave_1.users(id bigint, name varchar(8), comment varchar(16));
+
+insert into demo_master.users values(1,'KK01','master'),(2,'KK02','master'),(3,'KK03','master');
+insert into demo_slave_0.users values(1,'KK01','slave0'),(2,'KK02','slave0'),(3,'KK03','slave0');
+insert into demo_slave_1.users values(1,'KK01','slave1'),(2,'KK02','slave1'),(3,'KK03','slave1');
+
+
+
+
+create schema demo_ds_0;
+create schema demo_ds_1;
+
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_0 (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_1 (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_0 (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_1 (order_id BIGINT NOT NULL AUTO_INCREMENT, user_id INT NOT NULL, status VARCHAR(50), PRIMARY KEY (order_id));
+
+ insert into t_order(user_id,status) values(2,'ok'),(2,'fail');
+
